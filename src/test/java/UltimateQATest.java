89145820697a6473b89d20c9680583815ddddb70
„Monika <monikasmolgdy@gmail.com>
2023-05-28 14:44:02 +0200

    Second test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -16,7 +16,24 @@ public class UltimateQATest extends PageSetup {
 
         WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
        assertEquals("Button success", buttonSuccessText.getText());
+    }
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
+        nameTextField.sendKeys("Tester");
 
+        WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
+        emailTextField.sendKeys("tester@tester.com");
+
+        Thread.sleep(2000);
+
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement thanksText = driver.findElementByClassName("et-pb-contact-message");
+       assertEquals("Thanks for contacting us", thanksText.getText());
 
     }
 
