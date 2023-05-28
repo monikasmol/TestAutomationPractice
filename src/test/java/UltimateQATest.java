efa2e9c22868d7c3d5cfb24e26bfe881d7afa214
„Monika <monikasmolgdy@gmail.com>
2023-05-28 12:47:23 +0200

    Project created

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UltimateQATest extends PageSetup {
+
+    @Test
+    public void testOne() {
+
+
+
+    }
+
+
+}
