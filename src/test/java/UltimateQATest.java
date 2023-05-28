1915ff5d5b41ff87e0cd06e0b22963b08fbdd9f4
„Monika <monikasmolgdy@gmail.com>
2023-05-28 16:30:54 +0200

    Third test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UltimateQATest extends PageSetup {
 
@@ -36,6 +40,47 @@ public class UltimateQATest extends PageSetup {
        assertEquals("Thanks for contacting us", thanksText.getText());
 
     }
+
+    @Test
+    public void testThree() {
+        WebElement blueField = driver.findElementByXPath("//*[contains(@class, 'et_pb_module et_pb_cta_0 et_pb_promo')]");
+        assertEquals("rgba(46, 163, 242, 1)", blueField.getCssValue("background-color"));
+
+    }
+
+    @Test
+    public void testFour() {
+        WebElement bikeCheckbox = driver.findElementByXPath("//*[@type='checkbox' and @value='bike']");
+        bikeCheckbox.click();
+        assertTrue(bikeCheckbox.isSelected());
+    }
+
+    @Test
+    public void testFive() {
+
+        List<String> listOfCars = new ArrayList<>();
+        listOfCars.add("volvo");
+        listOfCars.add("saab");
+        listOfCars.add("opel");
+        listOfCars.add("audi");
+
+        for (int i = 0; i < listOfCars.size(); i++) {
+            WebElement dropdownOption = driver.findElementByXPath("//*[@value='" + listOfCars.get(i) + "']");
+            dropdownOption.click();
+            assertTrue(dropdownOption.isSelected());
+            //Java Stream od wersji 8
+        }
+
+
+    }
+
+
+
+
+     //div[@class='navbar-header']//img[@alt='Nasa'] - szukanie elementów w devtools
+     //*[text()='MORE STORIES']
+
+
 
 
 }
