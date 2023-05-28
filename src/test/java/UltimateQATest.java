22234f95c847e7ee241a324f72ecfba6405b8f03
„Monika <monikasmolgdy@gmail.com>
2023-05-28 14:04:13 +0200

    First test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,13 +1,21 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UltimateQATest extends PageSetup {
 
     @Test
     public void testOne() {
+        WebElement buttonUsingId = driver.findElementById("idExample");
+        buttonUsingId.click();
 
+        WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+       assertEquals("Button success", buttonSuccessText.getText());
 
 
     }
